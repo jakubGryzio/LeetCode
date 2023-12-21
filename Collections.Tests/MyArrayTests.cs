e09a269d98d4765eb0a8e34d49cf6a43b44c45b3
Jakub Gryzio <jakub.gryzio@hexagon.com>
2023-12-21 18:31:21 +0100

    Array algorithms implementation and setup tests

---
+++ Collections.Tests/MyArrayTests.cs
@@ -0,0 +1,249 @@
+﻿using Exercises.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Tests
+{
+    public class MyArrayTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Find_Pivot_Index()
+        {
+            int[] nums = { 1, 7, 3, 6, 5, 6 };
+            var pivotIndex = MyArray.PivotIndex(nums);
+
+            Assert.That(pivotIndex, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Find_No_Pivot_Index()
+        {
+            int[] nums = { 1, 2, 3 };
+            var pivotIndex = MyArray.PivotIndex(nums);
+
+            Assert.That(pivotIndex, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Find_Zero_Pivot_Index()
+        {
+            int[] nums = { 2, 1, -1 };
+            var pivotIndex = MyArray.PivotIndex(nums);
+
+            Assert.That(pivotIndex, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Dominant_Index()
+        {
+            int[] nums = { 3, 6, 1, 0 };
+            var dominantIndex = MyArray.DominantIndex(nums);
+
+            Assert.That(dominantIndex, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Non_Dominant_Index()
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            var dominantIndex = MyArray.DominantIndex(nums);
+
+            Assert.That(dominantIndex, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Zero_Dominant_Index()
+        {
+            int[] nums = { 0, 0, 0, 1 };
+            var dominantIndex = MyArray.DominantIndex(nums);
+
+            Assert.That(dominantIndex, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Zero_One_Dominant_Index()
+        {
+            int[] nums = { 1, 0 };
+            var dominantIndex = MyArray.DominantIndex(nums);
+
+            Assert.That(dominantIndex, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Zero_First_Dominant_Index()
+        {
+            int[] nums = { 0, 0, 3, 2 };
+            var dominantIndex = MyArray.DominantIndex(nums);
+
+            Assert.That(dominantIndex, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Plus_One()
+        {
+            int[] sourceNums = { 1, 2, 3 };
+            int[] nums = { 1, 2, 4 };
+            var plusOneArr = MyArray.PlusOne(sourceNums);
+
+            Assert.That(plusOneArr, Is.EquivalentTo(nums));
+        }
+
+        [Test]
+        public void Plus_One_4Digit()
+        {
+            int[] sourceNums = { 4,3,2,1 };
+            int[] nums = { 4, 3, 2, 2 };
+            var plusOneArr = MyArray.PlusOne(sourceNums);
+
+            Assert.That(plusOneArr, Is.EquivalentTo(nums));
+        }
+
+        [Test]
+        public void Plus_One_9Digit()
+        {
+            int[] sourceNums = { 9 };
+            int[] nums = { 1, 0 };
+            var plusOneArr = MyArray.PlusOne(sourceNums);
+
+            Assert.That(plusOneArr, Is.EquivalentTo(nums));
+        }
+
+        [Test]
+        public void Plus_One_Last_9Digit()
+        {
+            int[] sourceNums = { 3, 2, 9 };
+            int[] nums = { 3,3,0 };
+            var plusOneArr = MyArray.PlusOne(sourceNums);
+
+            Assert.That(plusOneArr, Is.EquivalentTo(nums));
+        }
+
+        [Test]
+        public void Plus_One_Last_99Digit()
+        {
+            int[] sourceNums = { 3, 9, 9 };
+            int[] nums = { 4, 0, 0 };
+            var plusOneArr = MyArray.PlusOne(sourceNums);
+
+            Assert.That(plusOneArr, Is.EquivalentTo(nums));
+        }
+
+        [Test]
+        public void Plus_One_Last_999Digit()
+        {
+            int[] sourceNums = { 9, 9, 9 };
+            int[] nums = { 1, 0, 0, 0 };
+            var plusOneArr = MyArray.PlusOne(sourceNums);
+
+            Assert.That(plusOneArr, Is.EquivalentTo(nums));
+        }
+
+        [Test]
+        public void Plus_One_Last_989Digit()
+        {
+            int[] sourceNums = { 9, 8, 9 };
+            int[] nums = { 9,9,0 };
+            var plusOneArr = MyArray.PlusOne(sourceNums);
+
+            Assert.That(plusOneArr, Is.EquivalentTo(nums));
+        }
+
+        [Test]
+        public void Plus_One_Invalid_Reversing() 
+        {
+            int[] sourceNums = { 9,8,7,6,5,4,3,2,1,0 };
+            int[] nums = { 9,8,7,6,5,4,3,2,1,1 };
+            var plusOneArr = MyArray.PlusOne(sourceNums);
+
+            Assert.That(plusOneArr, Is.EquivalentTo(nums));
+        }
+
+        [Test]
+        public void Plus_One_Long_Number()
+        {
+            int[] sourceNums = { 7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6 };
+            int[] nums = { 7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,7 };
+            var plusOneArr = MyArray.PlusOne(sourceNums);
+
+            Assert.That(plusOneArr, Is.EquivalentTo(nums));
+        }
+
+        [Test]
+        public void Find_Diagonal_Order()
+        {
+            int[][] nums = { new int[3] { 1, 2, 3 }, new int[3] { 4, 5, 6}, new int[3] { 7, 8 ,9} };
+            int[] numsArr = { 1, 2, 4, 7,5, 3, 6 ,8, 9 };
+            var output = MyArray.FindDiagonalOrder(nums);
+
+            Assert.That(numsArr, Is.EquivalentTo(output));
+        }
+
+        [Test]
+        public void Find_Diagonal_Order_II()
+        {
+            int[][] nums = { new int[2] { 1, 2 }, new int[2] { 3, 4 } };
+            int[] numsArr = { 1,2,3,4 };
+            var output = MyArray.FindDiagonalOrder(nums);
+
+            Assert.That(numsArr, Is.EquivalentTo(output));
+        }
+
+        [Test]
+        public void Find_Diagonal_Order_One_Digit()
+        {
+            int[][] nums = { new int[1] { 1 } };
+            int[] numsArr = { 1 };
+            var output = MyArray.FindDiagonalOrder(nums);
+
+            Assert.That(numsArr, Is.EquivalentTo(output));
+        }
+
+        [Test]
+        public void Find_Diagonal_Order_Two_Digit()
+        {
+            int[][] nums = { new int[2] { 2, 3 } };
+            int[] numsArr = { 2, 3 };
+            var output = MyArray.FindDiagonalOrder(nums);
+
+            Assert.That(numsArr, Is.EquivalentTo(output));
+        }
+
+        [Test]
+        public void Find_Diagonal_Order_Three_Digit()
+        {
+            int[][] nums = { new int[3] { 6, 9, 7 } };
+            int[] numsArr = { 6, 9, 7 };
+            var output = MyArray.FindDiagonalOrder(nums);
+
+            Assert.That(numsArr, Is.EquivalentTo(output));
+        }
+
+        [Test]
+        public void Find_Diagonal_Order_One_Column()
+        {
+            int[][] nums = { new int[1] { 6 }, new int[1] { 9 }, new int[1] { 7 } };
+            int[] numsArr = { 6, 9, 7 };
+            var output = MyArray.FindDiagonalOrder(nums);
+
+            Assert.That(numsArr, Is.EquivalentTo(output));
+        }
+
+        [Test]
+        public void Find_Diagonal_Order_Not_Equal_Dimension()
+        {
+            int[][] nums = { new int[2] { 2, 5 }, new int[2] { 8, 4 }, new int[2] { 0, -1 } };
+            int[] numsArr = { 2, 5, 8, 0, 4, -1 };
+            var output = MyArray.FindDiagonalOrder(nums);
+
+            Assert.That(numsArr, Is.EquivalentTo(output));
+        }
+    }
+}
---
+++ Collections/MyArray.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Collections
+{
+    public class MyArray
+    {
+        public static int PivotIndex(int[] nums)
+        {
+            int sumLeft = 0;
+            int sumRight = nums.Sum();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sumRight -= nums[i];
+
+                if (sumLeft == sumRight)
+                {
+                    return i;
+                }
+
+                sumLeft += nums[i];
+            }
+
+            return -1;
+        }
+
+        public static int DominantIndex(int[] nums)
+        {
+            int largestIndex = 0;
+            int secondLargest = 0;
+
+            for (int i = 1; i < nums.Length;i++)
+            {
+                if (nums[i] > nums[largestIndex])
+                {
+                    secondLargest = nums[largestIndex];
+                    largestIndex = i;
+                }
+                else if (nums[i] > secondLargest)
+                {
+                    secondLargest = nums[i];
+                }
+            }
+
+            return nums[largestIndex] >= 2 * secondLargest ? largestIndex : -1;
+        }
+
+        public static int[] PlusOne(int[] digits)
+        {
+            int n = digits.Length;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (digits[i] != 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+
+                digits[i] = 0;
+            }
+            //if all digits are 9 add 1 to start of sequence
+            return digits.Prepend(1).ToArray();
+        }
+
+        public static int[] FindDiagonalOrder(int[][] mat)
+        {
+            int m = mat.Length;
+            int n = mat[0].Length;
+            var output = new List<int>();
+            for (int i = 1; i <= n + m - 1; i++)
+            {
+                var diagonal = new List<int>();
+                for (int j = Math.Max(0, i - n); j < Math.Min(m, i); j++)
+                {
+                    diagonal.Add(mat[j][i - j - 1]);
+                }
+
+                if (i % 2 == 0)
+                {
+                    output.AddRange(diagonal);
+                }
+                else
+                {
+                    output.AddRange(diagonal.Reverse<int>());
+                }
+            }
+
+            return output.ToArray();
+        }
+    }
+}
